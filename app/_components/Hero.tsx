f3fb4dc83c2254da3e8ee65336dd3d615617a051
Ugo DASILVA <ugodasilva67@gmail.com>
2024-06-16 18:55:15 +0200

    create status section & add style on hero component

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -3,25 +3,33 @@ import { ComponentPropsWithoutRef} from "react";
 import {cn} from "@/lib/utils";
 
 const Code = ({className, ...props}: ComponentPropsWithoutRef<"span">) => {
-    return <span className={cn("bg-accent/30 font-mono text-primary border border-accent p-1 rounded-sm", className)} {...props} />
-}
+    return (
+        <span
+            className={cn(
+                "bg-accent/30 font-mono text-primary border border-accent p-1 rounded-sm",
+                className
+            )}
+            {...props}
+        />
+    );
+};
 
 export const Hero = () => {
     return (
-        <Section className="flex max-md:flex-col items-start">
-            <div className="flex-[2]">
+        <Section className="flex max-md:flex-col items-start gap-4">
+            <div className="flex-[2] flex flex-col gap-2">
                 <h2 className="font-caption text-5xl text-primary">Ugo DA SILVA</h2>
                 <h3 className="text-3xl font-caption">
                     Développeur Magento
                 </h3>
                 <p>
-                    lorem ipsum <Code>test</Code>
+                    lorem ipsum <Code>test code</Code>
                 </p>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 max-md:m-auto ml-auto">
                 <img
                     src="/ugo_profile.jpg"
-                    className="w-full h-auto max-w-xs"
+                    className="w-full h-auto max-w-xs max-md:w-56"
                     alt="ugo's profile"
                 />
             </div>
---
+++ app/_components/Status.tsx
@@ -0,0 +1,19 @@
+import { Section } from "./Section"
+
+export const Status= () => {
+    return (
+        <Section className="flex max-md:flex-col items-start gap-4">
+            <Card className="flex-[2] flex flex-col gap-2">
+                Side project
+            </Card>
+            <div className="flex-1">
+                <div>
+                    Work
+                </div>
+                <div>
+                    Contact Me
+                </div>
+            </div>
+        </Section>
+    )
+}
